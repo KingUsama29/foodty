beb2677f292429fd35f1c79db584390f77119191
Kingusama29 <usamagaming123@gmail.com>
2025-12-22 23:50:50 +0800

    feat: membuat tabel user, model user,  dan registerasi controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/register',[AuthController::class, 'register']);
